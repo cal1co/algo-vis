40dd3280702b5037062108d49e038281b63b9fee
Alex King <calicoalix@gmail.com>
2022-07-15 23:17:38 +1000

    added visual representation of start and target node

--- src/pathfinder/components/Pathfinder.tsx
+++ src/pathfinder/components/Pathfinder.tsx
@@ -4,10 +4,12 @@ import '../style/pathfinder.css'
 function Pathfinder() {
 
     const [nodes, setNodes] = useState(Array)
+    const [start, setStart] = useState(Object)
+    const [target, setTarget] = useState(Object)
 
     useEffect(() => {
         const nodesArr = []
-        for (let row = 0; row < 15; row++){
+        for (let row = 0; row < 21; row++){
             const currRow = []
             for (let col = 0; col < 35; col++){
                 currRow.push([])
@@ -16,13 +18,15 @@ function Pathfinder() {
         }
         setNodes(nodesArr)
         console.log(nodesArr)
+        setStart({row:10, col:7})
+        setTarget({row:10, col:27})
     },[setNodes])
 
     const renderBoard = () => {
         console.log('rendering board')
         return nodes.map((row:any, rowIdx:Number) => {
             return <div className="nodeRow" id={`${rowIdx}`} key={`${rowIdx}`}>{row.map((col:any, colIdx:any) => {
-                return <div className="node" id={`col${colIdx} row${rowIdx}`} key={`col${colIdx} row${rowIdx}`}></div>
+                return <div className={`node unvisited ${(colIdx === start.col) && (rowIdx === start.row) ? 'start' : (colIdx === target.col) && (rowIdx === target.row) ? 'target' : ''}`} id={`r${rowIdx}-c${colIdx}`} key={`col${colIdx} row${rowIdx}`}></div>
             })}</div>
                 
         })
