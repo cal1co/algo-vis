f25306365dcf9a3ad740fbe8f9597f000e065cc4
Alex King <calicoalix@gmail.com>
2022-07-16 01:37:35 +1000

    dijkstras works and shows shortest path - add walls next then A* and DFS + BFS

---
+++ src/pathfinder/algorithms/dijkstra.tsx
@@ -0,0 +1,78 @@
+export const dijkstra = (nodes:any, start:any, finish:any) => {
+    const visitedNodes:any = []
+    start.distance = 0
+    const unvisitedNodes = fetchNodes(nodes)
+    while (unvisitedNodes.length > 0){
+        sortNodesDist(unvisitedNodes)
+        const nextNode = unvisitedNodes.shift()
+        console.log(nextNode)
+        if (nextNode?.wall){
+            continue;
+        };
+        if (nextNode.distance === Infinity){
+            return visitedNodes
+        }
+        nextNode.visited = true
+        visitedNodes.push(nextNode)
+        if (nextNode === finish){
+            return visitedNodes
+        }
+        updateUnvisitedNeighbours(nextNode, nodes)
+    }
+}
+
+const fetchNodes = (grid:any) => {
+    const nodes:Array<any> = []
+    for (const row of grid){
+        for (const node of row){
+            nodes.push(node)
+        }
+    }
+    return nodes
+}
+
+const sortNodesDist = (unvisitedNodes:Array<any>) => {
+    unvisitedNodes.sort((a, b) => a.distance - b.distance)
+}
+
+const updateUnvisitedNeighbours = (node:any, grid:any) => {
+    const unvisitedNeighbours = getUnvisitedNeighbours(node, grid)
+    for (const neighbour of unvisitedNeighbours){
+        neighbour.distance = node.distance + 1
+        neighbour.prevNode = node
+    }
+}
+
+const getUnvisitedNeighbours = (node:any, grid:any) => {
+    const neighbours:any = []
+    const { row, column } = node
+    if (row > 0){
+        neighbours.push(grid[row - 1][column])
+    }
+    if (row < grid.length - 1){
+        neighbours.push(grid[row + 1][column])
+    }
+    if (column > 0) {
+        neighbours.push(grid[row][column - 1])
+    }
+    if (column < grid[0].length - 1){
+        neighbours.push(grid[row][column + 1])
+    }
+    return neighbours.filter((a:any) => !a.visited)
+}
+
+
+
+export const findShortestPathNodes = (target:any) => {
+    const shortestPathNodes:any = []
+    let currNode = target
+    console.log("CURRNODE", currNode)
+    console.log("SHORT", shortestPathNodes)
+    while (currNode !== null){
+        shortestPathNodes.unshift(currNode)
+        console.log("CURRNODE AGAIN!", currNode)
+
+        currNode = currNode.prevNode
+    }
+    return shortestPathNodes
+}
--- src/pathfinder/components/Pathfinder.tsx
+++ src/pathfinder/components/Pathfinder.tsx
@@ -1,26 +1,45 @@
 import { useState, useEffect } from 'react'
 import '../style/pathfinder.css'
+import { dijkstra, findShortestPathNodes } from '../algorithms/dijkstra'
 
 function Pathfinder() {
 
     const [nodes, setNodes] = useState(Array)
     const [start, setStart] = useState(Object)
     const [target, setTarget] = useState(Object)
+    
 
     useEffect(() => {
-        const nodesArr = []
+        setStart({row:10, col:7})
+        setTarget({row:10, col:27})
+        initGrid()
+    },[setNodes])
+
+    const initGrid = () => {
+        const nodesArr:Array<Object> = []
         for (let row = 0; row < 21; row++){
             const currRow = []
             for (let col = 0; col < 35; col++){
-                currRow.push([])
+                const currNode = createNode(row, col)
+                currRow.push(currNode)
             }
             nodesArr.push(currRow)
         }
         setNodes(nodesArr)
         console.log(nodesArr)
-        setStart({row:10, col:7})
-        setTarget({row:10, col:27})
-    },[setNodes])
+    }
+    const createNode = (row:Number, column:Number) => {
+        return {
+            row,
+            column,
+            startNode: row === start.row && column === start.col,
+            finishNode: row === target.row && column === target.col,
+            visited:false,
+            prevNode:null,
+            distance: Infinity,
+            wall:false,
+        }
+    }
 
     const renderBoard = () => {
         console.log('rendering board')
@@ -32,8 +51,48 @@ function Pathfinder() {
         })
     }
 
+
+    const animateNodesDijkstra = (visitedNodes:any, shortestPathNodes:any) => {
+        for (let i = 0; i <= visitedNodes.length; i++){
+            if (i === visitedNodes.length){
+                setTimeout(() => {
+                    animateShortestPath(shortestPathNodes)
+                }, 10 * i)
+                return 
+            }
+            setTimeout(() => {
+                const node = visitedNodes[i];
+                console.log(node.row, node.column)
+                document.getElementById(`r${node.row}-c${node.column}`)!.className = `node visited`
+            }, 10 * i)
+        }
+    }
+
+    const animateShortestPath = (shortestPathNodes:any) => {
+        for (let i = 0; i < shortestPathNodes.length; i++){
+            setTimeout(() => {
+                const node = shortestPathNodes[i]
+                document.getElementById(`r${node.row}-c${node.column}`)!.className = `node short-path`
+            }, 50 * i)
+        }
+    }
+
+
+    const showDijkstras = () => {
+        console.log("DIJKSTRAS HERE")
+        console.log(nodes)
+        const startIndex = nodes[start.row][start.col]
+        const targetIndex = nodes[target.row][target.col]
+        const visitedNodes = dijkstra(nodes, startIndex, targetIndex)
+        const shortestPathNodes = findShortestPathNodes(targetIndex)
+        animateNodesDijkstra(visitedNodes, shortestPathNodes)
+    }
+
     return (
         <div className="pathfinder">
+            <div className="pathfinder-nav">
+                <button className="dijkstra" onClick={showDijkstras}>Dijkstras</button>
+            </div>
             <div className="pathfind-board">
                 {renderBoard()}
             </div>
